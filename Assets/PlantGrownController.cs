d3ba5339682e4c822aaf7aa0807c993b0e85da4c
Artyom <60854544+Ortem-a@users.noreply.github.com>
2022-08-27 18:40:34 +0300

    Теперь растения могут расти

--- Assets/PlantGrownController.cs
+++ Assets/PlantGrownController.cs
@@ -13,25 +13,42 @@ public class PlantGrownController : Player
 
     public void ChangePlantState()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            
-            StartCoroutine(WaitTimeOfGrown(i)); // ожидание пока вырастет
-            //Destroy(_currentPrefab); // удаление старого префаба
-        }
-        //_currentPrefab = Instantiate(PlantPrefabs[2], transform); // спавн префаба растения
+        StartCoroutine(WaitTimeOfGrown1()); // ожидание пока вырастет
     }
 
-    private IEnumerator WaitTimeOfGrown(int index)
+    private IEnumerator WaitTimeOfGrown1()
     {
-        _currentPrefab = Instantiate(PlantPrefabs[index], transform); // спавн префаба растения
+        _currentPrefab = Instantiate(PlantPrefabs[0], transform); // спавн префаба растения
         _currentPrefab.transform.position += new Vector3(0, 0.4f, 0);
 
-        Debug.LogWarning($"WAIT FOR {TimeOfGrown} sec");
+        Debug.LogWarning($"1) WAIT FOR {TimeOfGrown} sec");
         yield return new WaitForSeconds(TimeOfGrown);
 
-        Destroy(_currentPrefab); // удаление старого префаб
-        ChangePlantState();
+        Destroy(_currentPrefab); // удаление старого префаба
+        StartCoroutine(WaitTimeOfGrown2()); // ожидание пока вырастет
+    }
+
+    private IEnumerator WaitTimeOfGrown2()
+    {
+        _currentPrefab = Instantiate(PlantPrefabs[1], transform); // спавн префаба растения
+        _currentPrefab.transform.position += new Vector3(0, 0.4f, 0);
+
+        Debug.LogWarning($"2) WAIT FOR {TimeOfGrown} sec");
+        yield return new WaitForSeconds(TimeOfGrown);
+
+        Destroy(_currentPrefab); // удаление старого префаба
+        StartCoroutine(WaitTimeOfGrown3()); // ожидание пока вырастет
+    }
+
+    private IEnumerator WaitTimeOfGrown3()
+    {
+        _currentPrefab = Instantiate(PlantPrefabs[2], transform); // спавн префаба растения
+        _currentPrefab.transform.position += new Vector3(0, 0.4f, 0);
+
+        Debug.Log($"3) WAIT FOR {TimeOfGrown} sec");
+        yield return new WaitForSeconds(TimeOfGrown);
+
+        Debug.Log($"PLANT {PlantPrefabs[2].name} ON {this.name} IS READY");
     }
 
     public void TakeReadyPlant()
